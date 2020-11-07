cbaf7fff3d8e90577b129087c9f9fc10822c6a6a
bellevin <bellevin@uqtr.ca>
2020-11-07 13:32:25 -0500

    Initial

---
+++ Devoir2/Matrix.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Devoir2
+{
+    class Matrix
+    {
+        private double[,] data;
+        private int rows;
+        private int cols;
+
+        public Matrix()
+        {
+        }
+
+        public Matrix(double[,] data)
+        {
+            this.data = data;
+            this.rows = data.GetLength(0);
+            this.cols = data.GetLength(1);
+        }
+
+        public Matrix(int rows, int cols)
+        {
+            this.rows = rows;
+            this.cols = cols;
+            this.data = new double[rows, cols];          
+        }
+
+        public double[,] getData()
+        {
+            return data; 
+        }
+
+        public void setData(double[,] data)
+        {
+            this.data = data;
+        }
+
+        public void display()
+        {
+            Console.WriteLine("Matrice: \n");
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < cols; j++)
+                {
+                    Console.Write(data[i, j] + "\t");
+                }
+                Console.WriteLine();
+            }
+        }
+
+        public Matrix addition(Matrix p_matrix)
+        {
+            if (rows == p_matrix.rows && cols == p_matrix.cols)
+            {
+                Matrix rm = new Matrix(rows,cols);
+
+                for (int i = 0; i < rows; i++)
+                    for (int j = 0; j < cols; j++)
+                        rm.data[i, j] = data[i, j] + p_matrix.data[i, j];
+
+                return rm;
+            }
+            return null;
+        }
+
+        public Matrix multiply(Matrix p_matrix)
+        {
+            if (cols != p_matrix.cols)
+            {
+                throw new InvalidOperationException ("Le nombre de colonnes de la première matrice doit etre égal au nombre de lignes de la deuxième matrice."); 
+            }
+            Matrix rm = new Matrix(rows,cols);
+            rm.data = new double[rows, p_matrix.cols];
+
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < p_matrix.cols; j++)
+                {
+                    for (int z = 0; z < cols; z++)
+                    {
+                        rm.data[i, j] += data[i, z] * p_matrix.data[z, j];
+                    }
+                }
+            }
+            return rm;
+        }
+
+        public Matrix scallarProduct(double p_nbr)
+        {
+            Matrix rm = new Matrix(rows, cols);
+            rm.data = new double[rows, cols];
+
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < cols; j++)
+                {
+                    rm.data[i, j] = data[i, j] * p_nbr;
+                }
+            }
+            return rm;
+        }
+
+        public void fillMatrix()
+        {
+            for (int i = 0; i < rows; ++i)
+            {
+                for (int j = 0; j < cols; ++j)
+                {
+                    data[i, j] = 0;
+                }
+            }
+        }
+
+
+    }
+}
---
+++ Devoir2/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Devoir2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Addition de matrices
+            Matrix m1 = new Matrix (new double[,]
+            {
+                    {5, 0, 2},
+                    {6, -4, 4},
+                    {1, 4, 1}
+            });
+
+            Matrix m2 = new Matrix(new double[,]
+            {
+                    {4, 1, 0},
+                    {4, -4, 3},
+                    {2, -8, 1}
+            });
+
+            m1.addition(m2).display();
+
+            //Test multiplication de matrices
+            Matrix m3 = new Matrix(new double[,]
+            {
+                    {2, 4},
+                    {-5, 1},
+                    {-1, 0}
+            });
+
+            Matrix m4 = new Matrix(new double[,]
+            {
+                    {8, 6},
+                    {-6, 7}
+            });
+
+            m3.multiply(m4).display();
+
+
+            //Test produit scallaire
+            Matrix m5 = new Matrix(new double[,]
+            {
+                    {5, 0, 2},
+                    {6, -4, 4},
+                    {1, 4, 1}
+            });
+
+            m5.scallarProduct(2).display();
+
+            Console.ReadLine();
+        }
+    }
+}
